0942290516257e11eb09a3e9fb16e0ac2ca6d2f2
Devans Suman <devanssuman22042gmail.com>
2026-04-09 16:05:54 +0530

    added user models

---
+++ src/Services/User/User/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace Users.API.Models
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = default!;
+        public string PasswordHash { get; set; } = default!;
+        public string Role { get; set; } = default!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
